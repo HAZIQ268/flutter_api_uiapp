fd801621ef349d867a6d1888052fbaaf74bb5987
Haziq <153829508+HAZIQ268@users.noreply.github.com>
2025-05-19 20:42:45 +0500

    Create register_auth

---
+++ uitask/lib/register_auth.dart
@@ -0,0 +1,184 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class register extends StatefulWidget {
+  register({super.key});
+
+  @override
+  State<register> createState() => _registerState();
+}
+
+class _registerState extends State<register> {
+  final TextEditingController name = new TextEditingController();
+  final TextEditingController email = new TextEditingController();
+  final TextEditingController password = new TextEditingController();
+
+  void add_data() async {
+
+try{
+      UserCredential users = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email.text, password: password.text);
+    
+    await FirebaseFirestore.instance.collection('flutterfirstdb').doc(users.user!.uid).set(
+      {
+        'id': users.user!.uid,
+        'name': name.text,
+        'email': email.text,
+        'password': password.text,
+      }
+    );
+    
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text('User Register Successfully 😊')));
+
+}
+catch(e){
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text('Error $e')));
+
+}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/');
+            },
+            icon: Icon(FontAwesomeIcons.arrowLeft, color: Colors.white)),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Register Account',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 30),
+            Container(
+              width: 300,
+              child: TextField(
+                controller: name,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    FontAwesomeIcons.user,
+                    color: Colors.amber,
+                    size: 18,
+                  ),
+                  labelText: 'Name',
+                  labelStyle: TextStyle(color: Colors.white70),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.amber),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Container(
+              width: 300,
+              child: TextField(
+                controller: email,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    FontAwesomeIcons.envelope,
+                    color: Colors.amber,
+                    size: 18,
+                  ),
+                  labelText: 'Email',
+                  labelStyle: TextStyle(color: Colors.white70),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.amber),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: 300,
+              child: TextField(
+                controller: password,
+                obscureText: true,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    FontAwesomeIcons.lock,
+                    color: Colors.amber,
+                    size: 18,
+                  ),
+                  labelText: 'Password',
+                  labelStyle: TextStyle(color: Colors.white70),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.amber),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Container(
+              width: 200,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amber,
+                  padding: const EdgeInsets.all(12),
+                ),
+                onPressed: () {
+                  add_data();
+                },
+                child: Text(
+                  'Register',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Text(
+                    'Already have an account?',
+                    style: TextStyle(color: Color.fromARGB(255, 148, 143, 143)),
+                  ),
+                ),
+                TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/login');
+                    },
+                    child: Text(
+                      'login',
+                      style: TextStyle(
+                        decoration: TextDecoration.underline,
+                      ),
+                    ))
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
