15ded94e3bb64d3df417e03b349e9abbf6f71e7b
asp <asp@aptech.com>
2025-05-23 19:37:31 +0500

    updated code

---
+++ uitask/lib/gitcheck.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class gitcheck extends StatefulWidget {
+  const gitcheck({super.key});
+
+  @override
+  State<gitcheck> createState() => _gitcheckState();
+}
+
+class _gitcheckState extends State<gitcheck> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
